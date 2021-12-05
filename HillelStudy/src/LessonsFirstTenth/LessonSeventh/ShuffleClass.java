d45f29a7a7010c3b9992ae1276ba73598a7407e5
DESKTOP-7PM46JA\virdi <butkohanna@gmail.com>
2021-12-05 23:45:21 +0200

    lesson(ten): update ShuffleClass.java and TaskOneLessonSeventh.java and add ErrorException.java

--- HillelStudy/src/LessonsFirstTenth/LessonSeventh/ShuffleClass.java
+++ HillelStudy/src/LessonsFirstTenth/LessonSeventh/ShuffleClass.java
@@ -1,25 +1,39 @@
 package LessonsFirstTenth.LessonSeventh;
 
+import LessonsFirstTenth.LessonNinth.Logger;
+import LessonsFirstTenth.LessonNinth.Status;
+
 import java.util.Random;
 
-public class ShuffleClass {
-    public void shuffle(String[] deck, String[] suites, String[] values, int noCards){
 
-        int count = 0;
-        for (String suite : suites) {
-            for (String value : values) {
-                deck[count++] = value + " of " + suite;
+public class ShuffleClass {
+    public void shuffle(String[] deck, String[] suites, String[] values, int noCards) throws RuntimeException {
+
+        try {
+            int count = 0;
+            for (String suite : suites) {
+                for (String value : values) {
+                    deck[count++] = value + " of " + suite;
+                }
             }
+        } catch (Exception deckError) {
+            Logger.setStatus(Status.ERROR);
+            System.out.println(Logger.getStatus() + "Wrong deck calculation");
         }
 
         Random random = new Random();
-        for (int i = 0; i < 10; i++) {
-            int in = random.nextInt(noCards);
-            int out = random.nextInt(noCards);
+        try {
+            for (int i = 0; i < 10; i++) {
+                int in = random.nextInt(noCards);
+                int out = random.nextInt(noCards);
 
-            String cards = deck[in];
-            deck[in] = deck[out];
-            deck[out] = cards;
+                String cards = deck[in];
+                deck[in] = deck[out];
+                deck[out] = cards;
+            }
+        } catch (Exception cardsError) {
+            Logger.setStatus(Status.ERROR);
+            System.out.println(Logger.getStatus() + "Wrong cards calculation");
         }
     }
 }
---
+++ HillelStudy/src/LessonsFirstTenth/LessonTenth/ErrorException.java
@@ -0,0 +1,7 @@
+package LessonsFirstTenth.LessonTenth;
+
+public class ErrorException extends RuntimeException{
+    public ErrorException(String message) {
+        super(message);
+    }
+}
