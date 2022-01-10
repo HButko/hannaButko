a738b772f6b522d4a4161e46ef7b9072a4e47b0d
DESKTOP-7PM46JA\virdi <butkohanna@gmail.com>
2022-01-10 16:14:40 +0200

    project(upd): update dependency

--- src/test/java/tests/LessonTwenty/testBinWebDriver.java
+++ src/test/java/tests/LessonTwenty/testBinWebDriver.java
@@ -1,5 +1,6 @@
 package tests.LessonTwenty;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -9,6 +10,17 @@ public class testBinWebDriver {
     @Test
     private void webDriverTest() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/bin/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
+        driver.quit();
+    }
+
+    @Test
+    private void wdTest() {
+        WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver();
 
