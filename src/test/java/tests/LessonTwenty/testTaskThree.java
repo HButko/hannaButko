bf415a31b4fb3c71a465b38f96e3c3d6210741b8
DESKTOP-7PM46JA\virdi <butkohanna@gmail.com>
2022-01-16 20:35:05 +0200

    project(test): update task with WebDriverWaits

--- src/test/java/tests/LessonTwenty/testTaskThree.java
+++ src/test/java/tests/LessonTwenty/testTaskThree.java
@@ -4,8 +4,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;K
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 // https://the-internet.herokuapp.com/geolocation - кликнуть на кнопку, и после клика - достать значения координат,
@@ -13,26 +17,35 @@ import org.testng.annotations.Test;
 // Проще всего сделать через Thread.sleep(1000) (также нужно обработать исключение)
 
 public class testTaskThree {
+    private WebDriver driver;
+
+    @BeforeTest
+    private void beforeTest() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
 
     @Test
-    private void locationLocatorTest() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+    public void locationLocatorTest() {
         driver.get("https://the-internet.herokuapp.com/geolocation");
+        WebDriverWait driverWait = new WebDriverWait(driver, 5);
 
         WebElement button = driver.findElement(By.xpath("//button"));
         button.click();
 
-        Thread.sleep(1000);
+        driverWait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div#map-link")));
 
         WebElement resultLatValue = driver.findElement(By.xpath("//div[@id='lat-value']"));
         WebElement resultLongValue = driver.findElement(By.xpath("//div[@id='long-value']"));
 
-//        System.out.printf("\n Latitude: " + resultLatValue.getText() + "\n Longitude: " + resultLongValue.getText());
+        System.out.printf("\n Latitude: " + resultLatValue.getText() + "\n Longitude: " + resultLongValue.getText());
 
         Assert.assertTrue(resultLatValue.getText().contains("50.4758272"), "Invalid latitude");
         Assert.assertTrue(resultLongValue.getText().contains("30.4381952"), "Invalid longitude");
+    }
 
+    @AfterTest
+    private void afterTest() {
         driver.quit();
     }
 }
