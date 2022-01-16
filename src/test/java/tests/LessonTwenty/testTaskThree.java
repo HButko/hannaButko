a98020141e4e5d2b28ab7ee05e80e7ce6c5cb4b4
DESKTOP-7PM46JA\virdi <butkohanna@gmail.com>
2022-01-16 20:35:34 +0200

    project(test): update task with WebDriverWaits

--- src/test/java/tests/LessonTwenty/testTaskThree.java
+++ src/test/java/tests/LessonTwenty/testTaskThree.java
@@ -4,7 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;K
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
