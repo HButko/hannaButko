4a216192fc8e6f126162c2666b43efc00f5bd84a
DESKTOP-7PM46JA\virdi <butkohanna@gmail.com>
2022-03-15 00:00:17 +0200

    project(update): update tests with taking screenshots

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,11 +1,21 @@
 package tests;
 
+import com.codeborne.selenide.commands.TakeScreenshot;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import pageObjects.MainPage;
 import utils.DriverSingleton;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class BaseTest {
     protected WebDriver webDriver;
@@ -20,6 +30,14 @@ public class BaseTest {
         webDriver.manage().deleteAllCookies();
     }
 
+    @AfterMethod
+    public void makeScreenshot(ITestResult result) throws IOException {
+        if(!result.isSuccess()) {
+            File screenshot = ((TakesScreenshot)webDriver).getScreenshotAs(OutputType.FILE);
+            saveScreenshot(Files.readAllBytes(screenshot.toPath()));
+        }
+    }
+
     @AfterTest
     public void tearDown() {
         if (this.webDriver != null) {
@@ -31,5 +49,10 @@ public class BaseTest {
         webDriver.get("https://the-internet.herokuapp.com/");
 
         return new MainPage();
+    }
+
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] saveScreenshot(byte[] screenShot) {
+        return screenShot;
     }
 }
